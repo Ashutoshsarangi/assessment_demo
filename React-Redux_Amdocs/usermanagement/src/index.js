cc235992ac9ae67c472083c1a21bb6112f37d888
Ashu <ashutoshsarangi95@gmail.com>
2021-10-06 11:17:15 +0530

    Added Redux dev tool .

--- React-Redux_Amdocs/usermanagement/src/index.js
+++ React-Redux_Amdocs/usermanagement/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import reducer from './Store/reducer';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(reducer, composeEnhancers(
+    applyMiddleware(thunk)
+  ));
 
 
 
